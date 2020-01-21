6dd1a383250347ba6c9ad7c3a888a187398fd53e
Chance Strickland <hi@chancedigital.io>
2020-01-21 12:08:55 -0800

    write da machine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import "./App.css";
 
 /**
@@ -30,29 +30,49 @@ let url = `https://my-json-server.typicode.com/${user}/${repo}/posts/1`;
 function App() {
   let [data, setData] = useState(null);
   let [error, setError] = useState(null);
+  let [loading, setLoading] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        {data && (
-          <pre
-            style={{
-              maxWidth: 300
+        <Card>
+          <Message
+            heading={
+              data
+                ? "Thank you for signing up!"
+                : error
+                ? "Uh oh, something went wrong!"
+                : "Sign up for our newsletter"
+            }
+          >
+            <MessageText data={data} error={error} loading={loading} />
+          </Message>
+          <button
+            className="button fetch-button"
+            onClick={() => {
+              setLoading(true);
+              fetch(url)
+                .then(res => {
+                  if (res.status !== 200) {
+                    throw new Error(res.statusText);
+                  }
+                  if (getRandomInt(3) === 1) {
+                    throw new Error("Please try your submission again later.");
+                  }
+                  return res.json();
+                })
+                .then(data => {
+                  setLoading(false);
+                  setData(data.title);
+                })
+                .catch(err => {
+                  setLoading(false);
+                  setError(err.message);
+                });
             }}
           >
-            {JSON.stringify(data)}
-          </pre>
-        )}
-        <button
-          className="button fetch-button"
-          onClick={() => {
-            fetch(url)
-              .then(res => res.json())
-              .then(data => setData(data))
-              .catch(err => setError(err));
-          }}
-        >
-          Get My Data!
-        </button>
+            Get My Data!
+          </button>
+        </Card>
         <button
           className="button reset-button"
           onClick={() => {
@@ -67,4 +87,37 @@ function App() {
   );
 }
 
+function Card(props) {
+  return <div className="card" {...props} />;
+}
+
+function Message({ heading, children, ...props }) {
+  return (
+    <div className="message" {...props}>
+      <h3>{heading}</h3>
+      {children}
+    </div>
+  );
+}
+
+function MessageText({ data, loading, error }) {
+  return (
+    <Fragment>
+      {!data && !loading && <p>Let's get some data</p>}
+      {loading && <p>Loading...</p>}
+      {data && <p>{data}</p>}
+      {error && <p>{error}</p>}
+    </Fragment>
+  );
+}
+
 export default App;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// IDLE
+// LOADING
+// ERROR
+// SUCCESS
---
+++ src/state-machine.js
@@ -0,0 +1,337 @@
+import { useState, useEffect, useRef } from "react";
+
+/*
+It's helpful to start any project using a state machine by defining our possible
+states
+*/
+const NOT_STARTED = "NOT_STARTED";
+const RUNNING = "RUNNING";
+const STOPPED = "STOPPED";
+
+/*
+possible actions
+
+{ type: 'ASSIGN_CONTEXT', assign: (nextContext: Partial<ContextObj>, eventObject<EventObj>) => ContextObj }
+
+ */
+
+/*
+machineDefinition = {
+  initial: StateKey,
+  actions: { type: ActionType; exec?(context: Context, event: Event): void }[]
+  states: {
+    [key: StateKey]: {
+      on: {
+        [key: EventType]: {
+          target: StateKey,
+          actions?: func[],
+          cond?(context: Context, event: Event): boolean
+        }
+      }
+    },
+  }
+}
+*/
+
+export function createMachine(machineDefinition) {
+  let machine = {
+    initialState: {
+      value: machineDefinition.initial,
+      actions: machineDefinition.states[machineDefinition.initial].entry,
+      context: machineDefinition.context
+    },
+    transition(currentState /* string */, event /* { type: string; ... } */) {
+      /*
+      First thing's first, we need to define our state object with the current
+      state's value, its associated contextual data, and any actions we need to
+      perform. We'll always return a state object with our transition, so we'll
+      call this our `unchangedState` we can use if no transitions match our
+      definition.
+       */
+      let context = machineDefinition.context;
+      let unchangedState = {
+        value: currentState,
+        context,
+        actions: []
+      };
+
+      /*
+      Get the full definition of our current state and the requested transition
+      from our machine definition. If the transition isn't defined, we can bail
+      out and return the unchanged state object.
+      */
+      let stateDefinition = machineDefinition.states[currentState];
+      let transitionDefinition = stateDefinition.on[event.type];
+
+      if (!transitionDefinition) {
+        return unchangedState;
+      }
+
+      /*
+      Now that we have the transition defined, we need to figure out:
+        1) what is our target state?
+        2) what conditions need to be met for the transition to be successful?
+      */
+      let target = transitionDefinition.target || currentState;
+
+      /*
+      The conditional function should return true if the transition is allowed,
+      and false if not.
+      */
+      function cond(...args) {
+        if (typeof transitionDefinition.cond === "function") {
+          return transitionDefinition.cond(...args);
+        } else {
+          return true;
+        }
+      }
+
+      let nextContext = context;
+
+      /*
+      Check if the condition is met.
+      If no transition match, just return the state unchanged
+      */
+      if (!cond(context, event)) {
+        return unchangedState;
+      }
+
+      /*
+      Next we need to figure out all the actions we need to take.
+        1) Our current state exits, so fire off its exit actions
+        2) Our transition occurs, so fire off all transition actions.
+        3) Our next state enters, so fire off its entry actions.
+      */
+      let nextStateDefinition = machineDefinition.states[target];
+      let currentStateExitActions = stateDefinition.exit || [];
+      let transitionActions = transitionDefinition.actions || [];
+      let nextStateEntryActions = nextStateDefinition.entry || [];
+
+      /*
+      Package up all of our actions in the correct order.
+      */
+      let allActions = [
+        ...currentStateExitActions,
+        ...transitionActions,
+        ...nextStateEntryActions
+      ];
+
+      /*
+      We treat assignment actions differently than any other actions because we
+      need the updated context data in our updated state object. All other
+      actions are stored in the actions key of the state object and fired off in
+      order when the service starts and an event is delivered.
+
+      Instead of remembering and using this key in our app, we'll export an
+      `assign` function that handles this for us.
+      */
+      let actions = allActions.filter(action => {
+        if (action.type === "ASSIGN_CONTEXT") {
+          let tempContext = {
+            ...nextContext,
+            ...action.assignment(nextContext, event)
+          };
+          nextContext = tempContext;
+          return false;
+        }
+        return true;
+      });
+
+      return {
+        value: target,
+        context: nextContext,
+        actions
+      };
+    }
+  };
+  return machine;
+}
+
+/*
+From the XState docs:
+
+While a state machine/statechart with a pure .transition() function is useful
+for flexibility, purity, and testability, in order for it to have any use in a
+real-life application, something needs to:
+    - Keep track of the current state, and persist it
+    - Execute side-effects
+    - Handle delayed transitions and events
+    - Communicate with external services
+
+The interpreter is responsible for interpreting the state machine/statechart
+and doing all of the above. We need to parse and execute our machine it in a
+runtime environment. An interpreted, running instance of a statechart is called
+a service.
+
+This function takes a machine as its argument and returns a service that we can
+start and stop so that we can use it to send events and kick of transitions.
+ */
+function interpret(machine) {
+  /*
+  Set our initial state for the service. Our state machine uses the same concept
+  of finite states under the hood. How cool!
+  */
+  let status = NOT_STARTED;
+
+  /*
+  Get our current state and set up an object where we can store listeners.
+  When an event is fired, whatever callback we create as a listener when we
+  subscribe to the service will be updated any time our state changes.
+  This will be how our `useMachine` hook communicates with our state machine and
+  updates local state in React as events are sent!
+  */
+  let state = machine.initialState;
+  let listeners = new Set();
+
+  /*
+  Define our state machine service object with the following functions:
+    - send
+    - subscribe
+    - start
+    - stop
+  */
+  let service = {
+    /*
+    The send function is where we pass messages to our machine so that it can
+    fire transitions and actions.
+     */
+    send(event) {
+      // Only send events if our machine is running
+      if (status !== RUNNING) {
+        return;
+      }
+
+      /*
+      Update our state by calling a transition to find the new state based on
+      the event we send.
+       */
+      state = machine.transition(state, event);
+
+      /*
+      Now that we have our new state, we can start executing all of the actions
+      we stored in the state object in our `createMachine` function in the
+      correct order.
+      */
+      for (let action of state.actions) {
+        action.exec(state.context, event);
+      }
+
+      /*
+      After firing our actions, we call the listener functions set up any time a
+      subscription is called.
+      */
+      for (let listener of listeners) {
+        listener(state);
+      }
+    },
+
+    /*
+    An app can subscribe to state changes and do something with the updated
+    state via a listener function. This is key for interpretting our transition
+    for use in React (or any other framework/application).
+    */
+    subscribe(listener) {
+      /*
+      Add the listener to our store and immediately call it with the current
+      state.
+      */
+      listeners.add(listener);
+      listener(state);
+
+      // Return a clean up function so our app can unsubscribe
+      return {
+        unsubscribe() {
+          listeners.delete(listener);
+        }
+      };
+    },
+
+    /*
+    This function starts our machine so that everything works as expected.
+    */
+    start() {
+      // Update our status state to indicate that our machine is running
+      status = RUNNING;
+      for (let action of state.actions) {
+        /*
+        Fire actions with an initializer event to kick off any immediate
+        transitions update context, or perform any side effects defined by our
+        machine.
+        */
+        action.exec(state.context, {
+          type: "STATE_MACHINE_INIT"
+        });
+      }
+      // Return our service for use by our app.
+      return service;
+    },
+    /*
+    This function stops our machine completely, cleans up our listeners and
+    prevents new events from changing the state.
+    */
+    stop() {
+      // Update the state of our machine to STOPPED
+      status = STOPPED;
+      // Remove all of our listeners from the store.
+      for (let listener of listeners) {
+        listeners.delete(listener);
+      }
+      // Return our service for, we can always restart it again later!
+      return service;
+    }
+  };
+  return service;
+}
+
+export function useMachine(initialMachine) {
+  /*
+  Our state machine and the service from our interpreter should not change
+  between renders. We create a `useConstant` function to guarantee that the
+  values will never be recalculated. (useMemo does not offer that guarantee!)
+  https://github.com/Andarist/use-constant
+  https://reactjs.org/docs/hooks-faq.html#how-to-create-expensive-objects-lazily
+  */
+  const machine = useConstant(() => initialMachine);
+  const service = useConstant(() => interpret(machine).start());
+
+  // Store our machine state in React state
+  const [current, setCurrent] = useState(machine.initialState);
+
+  useEffect(() => {
+    /*
+    When the component is up and running, subscribe to our state machine and use
+    `setCurrent` as our listener to keep our component's state aligned with the
+    state machine. Return a cleanup function to stop the service when the
+    component unmounts.
+    */
+    service.subscribe(setCurrent);
+    return () => {
+      service.stop();
+    };
+  }, [service]);
+
+  /*
+  Our hook provides components with the current state object, and our service's
+  send function to send events to the machine.
+  */
+  return [current, service.send];
+}
+
+/*
+ Our `assign` function is an action creator to tell our state machine that we're
+ performing an action to update contextual data.
+ */
+export function assign(assignment) {
+  return {
+    type: "ASSIGN_CONTEXT",
+    assignment
+  };
+}
+
+function useConstant(fn) {
+  const ref = useRef();
+  if (!ref.current) {
+    ref.current = { v: fn() };
+  }
+  return ref.current.v;
+}
